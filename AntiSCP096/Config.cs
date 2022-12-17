fea8f70e8ceb9b83c3962b492d6d5f5122868819
XLittleLeft <XLittleLeft@Gmail.com>
2022-12-17 22:08:01 +0800

    delete something

--- AntiSCP096/Config.cs
+++ AntiSCP096/Config.cs
@@ -7,7 +7,5 @@ namespace AntiSCP096
     {
         [Description("取消生成SCP096")]
         public bool AntiSCP096 { get; set; } = true;
-        [Description("将变成SCP096的人变成啥")]
-        public RoleTypeId RoleTypeId { get; set; } = RoleTypeId.FacilityGuard;
     }
 }
