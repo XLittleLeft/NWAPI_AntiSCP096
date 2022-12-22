ebefdd3a2af4e5cc2f78bc2d122e5a4f21f6fdbe
XLittleLeft <XLittleLeft@Gmail.com>
2022-12-22 18:08:37 +0800

    fix same scp

---
+++ AntiSCP096/Anti.cs
@@ -0,0 +1,16 @@
+﻿using PlayerRoles;
+using System;
+using System.Collections.Generic;
+
+namespace AntiSCP096
+{
+    public class Anti
+    {
+        public static RoleTypeId GetRandom(List<RoleTypeId> roleTypeIds)
+        {
+            Random random = new Random();
+            int index = random.Next(roleTypeIds.Count);
+            return roleTypeIds[index];
+        }
+    }
+}
--- AntiSCP096/Plugin.cs
+++ AntiSCP096/Plugin.cs
@@ -5,33 +5,59 @@ using PluginAPI.Core.Attributes;
 using PluginAPI.Enums;
 using PluginAPI.Events;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Log = PluginAPI.Core.Log;
+using Random = UnityEngine.Random;
 
 namespace AntiSCP096
 {
     public class Plugin
     {
-        [PluginEntryPoint("AntiSCP096","Avoid the bug of SCP096","1.0.0","X小左(XLittleLeft)")]
+
+        List<RoleTypeId> roles = new List<RoleTypeId> { RoleTypeId.Scp079,RoleTypeId.Scp049,RoleTypeId.Scp106
+        ,RoleTypeId.Scp173,RoleTypeId.Scp939};
+
+        [PluginEntryPoint("AntiSCP096","Avoid the bug of SCP096","1.0.1","X小左(XLittleLeft)")]
         void LoadPlugin()
         {
             EventManager.RegisterEvents(this);
         }
+
         [PluginEvent(ServerEventType.PlayerSpawn)]
         void onPlayerSpawn(Player player,RoleTypeId roleTypeId)
         {
-            Timing.CallDelayed(1f, () =>
+            Timing.CallDelayed(0.5f, () =>
+            {
+                if (roleTypeId == RoleTypeId.Scp106)
+                    roles.Remove(RoleTypeId.Scp106);
+                if (roleTypeId == RoleTypeId.Scp079)
+                    roles.Remove(RoleTypeId.Scp079);
+                if (roleTypeId == RoleTypeId.Scp173)
+                    roles.Remove(RoleTypeId.Scp173);
+                if (roleTypeId == RoleTypeId.Scp939)
+                    roles.Remove(RoleTypeId.Scp939);
+                if (roleTypeId == RoleTypeId.Scp049)
+                    roles.Remove(RoleTypeId.Scp049);
+            });
+
+            Timing.CallDelayed(2f, () =>
             {
                 if (roleTypeId == RoleTypeId.Scp096)
                 {
                     Log.Debug("Changed");
-                    player.SetRole((RoleTypeId)UnityEngine.Random.Range
+                    /*player.SetRole((RoleTypeId)Random.Range
                         (0, Enum.GetValues(typeof(RoleTypeId)).ToArray<RoleTypeId>().Count
                         (x => x != RoleTypeId.Scp096 && x.ToString().Contains("scp") && 
-                        ReferenceHub.AllHubs.All(y => y.roleManager.CurrentRole.RoleTypeId != x))));
+                        ReferenceHub.AllHubs.All(y => y.roleManager.CurrentRole.RoleTypeId != x))));*/
+                    if (roles.Count > 0)
+                    player.SetRole(Anti.GetRandom(roles));
+                    else
+                        player.SetRole(RoleTypeId.FacilityGuard);
                 }
             });
         }
+
         [PluginConfig]
         public Config Config;
     }
